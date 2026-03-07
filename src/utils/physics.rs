d0bd4321fb6d927f1aa15b28a4d0db8402c0ea89
Max Generowicz <mdcg-github@my-post-office.net>
2026-03-07 19:51:49 +0000

    Panic when calling gamma() on gammas

--- src/utils/physics.rs
+++ src/utils/physics.rs
@@ -7,11 +7,11 @@ pub fn energy(particle: &Particle) -> f64 {
 }
 
 // Get gamma factor of particle
-pub fn gamma(particle: &Particle) -> Option<f64> {
+pub fn gamma(particle: &Particle) -> f64 {
     let energy = energy(&particle);
     match particle.species {
-        ParticleType::Gamma => None,
-        _                   => Some(energy / particle.state.m),
+        ParticleType::Gamma => panic!("gamma factor undefined for photons"),
+        _                   => energy / particle.state.m,
     }
 }
 
@@ -19,7 +19,7 @@ pub fn gamma(particle: &Particle) -> Option<f64> {
 pub fn beta(particle: &Particle) -> f64 {
     match particle.species {
         ParticleType::Gamma => 1.0,
-        _                   => (1.0 - 1.0/gamma(&particle).expect("Division by gamma factor which is equal to 0.").powf(2.0)).sqrt(),
+        _                   => (1.0 - 1.0/gamma(&particle).powf(2.0)).sqrt(),
     }
 }
 
@@ -80,16 +80,13 @@ mod tests {
     fn test_physics_gamma() {
         let p1 = Particle::new(Vec3(1.0, -1.0, 0.0), Vec3(4.0, -3.0, 0.0), ParticleType::Electron);
         let p2 = Particle::new(Vec3(0.0, 3.0, 5.0), Vec3(5.6, -2.1, -1.3), ParticleType::Muon);
-        let p3 = Particle::new(Vec3(0.2, 2.5, -0.1), Vec3(5.2, -3.1, 4.4), ParticleType::Gamma);
-        let p4 = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), ParticleType::Electron);
-        let p5 = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), ParticleType::Muon);
-        let p6 = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), ParticleType::Gamma);
-        assert_gamma_eq(gamma(&p1), Some(9.835703071628355));
-        assert_gamma_eq(gamma(&p2), Some(1.001676303733957));
-        assert_gamma_eq(gamma(&p3), None);
-        assert_gamma_eq(gamma(&p4), Some(1.0));
-        assert_gamma_eq(gamma(&p5), Some(1.0));
-        assert_gamma_eq(gamma(&p6), None);
+        let p3 = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), ParticleType::Electron);
+        let p4 = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), ParticleType::Muon);
+        assert_relative_eq!(gamma(&p1), 9.835703071628355);
+        assert_relative_eq!(gamma(&p2), 1.001676303733957);
+        assert_relative_eq!(gamma(&p3), 1.0);
+        assert_relative_eq!(gamma(&p4), 1.0);
+        
     }
 
     #[test]
