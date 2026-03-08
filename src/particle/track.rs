5fed940f94f4b7a56805e294c95b225e995f786b
Max Generowicz <mdcg-github@my-post-office.net>
2026-03-08 15:07:10 +0000

    Add TrackPoint creation test

--- src/particle/track.rs
+++ src/particle/track.rs
@@ -32,5 +32,24 @@ impl Track {
         self.points.push(TrackPoint::new(position, time, energy_deposit));
     }
 }
+
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::assert_vec3_eq;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn test_trackpoint_creation() {
+        let tp1 = TrackPoint::new(Vec3(0.0, 0.0, 0.0), 0.0, None);
+        let tp2 = TrackPoint::new(Vec3(1.7, -3.2, -5.0), 4.2, Some(13.5));
+        assert_vec3_eq!(tp1.r, Vec3(0.0, 0.0, 0.0));
+        assert_relative_eq!(tp1.t, 0.0);
+        assert_eq!(tp1.E, None);
+        assert_vec3_eq!(tp2.r, Vec3(1.7, -3.2, -5.0));
+        assert_relative_eq!(tp2.t, 4.2);
+        assert_eq!(tp2.E, Some(13.5));
     }
 }
