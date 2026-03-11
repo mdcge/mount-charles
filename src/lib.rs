152a2493250ea4fc385bd8d260cf23695a049c4b
Max Generowicz <mdcg-github@my-post-office.net>
2026-03-11 20:38:39 +0000

    Add specific struct exports to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@ pub mod utils;
 pub mod particle;
 pub mod geometry;
 pub mod sim;
+
+pub use particle::particle::{Particle, ParticleType};
+pub use particle::track::{Track, TrackPoint};
+pub use geometry::volume::Volume;
+pub use sim::world::World;
+pub use utils::vec3::Vec3;
