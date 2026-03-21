74c2f9bd2eebcafeba9dda3aa86241174c5d37a1
Max Generowicz <mdcg-github@my-post-office.net>
2026-03-21 15:33:20 +0000

    Add absorption and scattering lengths to Volume

--- src/geometry/volume.rs
+++ src/geometry/volume.rs
@@ -8,11 +8,13 @@ pub struct Volume {
     pub X0: f64,    // radiation length (mm)
     pub LY: f64,    // light yield (photons / MeV)
     pub n: f64,     // refractive index
+    pub la: f64,    // absorption length (mm)
+    pub ls: f64,    // scattering length (mm)
 }
 
 impl Volume {
-    pub fn new(s: f64, radiation_length: f64, light_yield: f64, refractive_index: f64) -> Self {
-        Volume { size: s, X0: radiation_length, LY: light_yield, n: refractive_index }
+    pub fn new(s: f64, radiation_length: f64, light_yield: f64, refractive_index: f64, absorption_length: f64, scattering_length: f64) -> Self {
+        Volume { size: s, X0: radiation_length, LY: light_yield, n: refractive_index, la: absorption_length, ls: scattering_length }
     }
 
     pub fn contains(&self, particle: &Particle) -> bool {
@@ -71,9 +73,9 @@ mod tests {
 
     #[test]
     fn test_volume_creation() {
-        let v1 = Volume::new(5.0, 50.0, 200.0, 1.2);
-        let v2 = Volume::new(15.0, 36.0, 150.0, 1.5);
-        let v3 = Volume::new(62.3, 52.0, 300.0, 1.4);
+        let v1 = Volume::new(5.0, 50.0, 200.0, 1.2, 2000.0, 2000.0);
+        let v2 = Volume::new(15.0, 36.0, 150.0, 1.5, 2500.0, 1500.0);
+        let v3 = Volume::new(62.3, 52.0, 300.0, 1.4, 1500.0, 1000.0);
         assert_relative_eq!(v1.size, 5.0);
         assert_relative_eq!(v2.size, 15.0);
         assert_relative_eq!(v3.size, 62.3);
@@ -81,8 +83,8 @@ mod tests {
 
     #[test]
     fn test_volume_contains() {
-        let v1 = Volume::new(10.0, 30.0, 110.0, 1.7);
-        let v2 = Volume::new(28.4, 44.2, 130.0, 1.0);
+        let v1 = Volume::new(10.0, 30.0, 110.0, 1.7, 1000.0, 800.0);
+        let v2 = Volume::new(28.4, 44.2, 130.0, 1.0, 1200.0, 1500.0);
         let p1 = Particle::new(Vec3(1.0, 2.0, -3.0), Vec3(5.0, 0.0, 0.0), ParticleType::Electron);
         let p2 = Particle::new(Vec3(4.2, -1.5, 5.1), Vec3(3.4, -2.0, 0.7), ParticleType::Muon);
         let p3 = Particle::new(Vec3(20.1, -10.3, -9.7), Vec3(-100.0, 0.0, -52.1), ParticleType::Gamma);
@@ -96,8 +98,8 @@ mod tests {
 
     #[test]
     fn test_intersect() {
-        let v1 = Volume::new(100.0, 30.0, 110.0, 1.3);
-        let v2 = Volume::new(150.0, 44.2, 130.0, 1.54);
+        let v1 = Volume::new(100.0, 30.0, 110.0, 1.3, 2000.0, 1800.0);
+        let v2 = Volume::new(150.0, 44.2, 130.0, 1.54, 5000.0, 3000.0);
         let p1 = Vec3::new(0.0, 0.0, 0.0);
         let p2 = Vec3::new(40.0, 30.0, -20.0);
         let d1 = Vec3::new(1.0, 0.0, 0.0);
